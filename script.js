052e7da12fae787e748ac4ba898a7c1d38c6bbcb
ClementTANGUY <clemdelatang@gmail.com>
2023-04-07 11:32:49 +0200

    Good Vercel backend URL

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-fetch(`https://whatyearisit-backend-clementtanguy.vercel.app/year/`)
+fetch(`https://whatyearisit-backend-dun.vercel.app/year/`)
   .then(response => response.json())
   .then(data => {
     document.querySelector('#year').textContent = data.year;
